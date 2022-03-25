434151908e80c9978bdaab295893573be10ac23d
EduardoGC715 <99993140+EduardoGC715@users.noreply.github.com>
2022-03-24 20:44:11 -0600

    Modificación del Agente Base
    Se agregaron atributos, un constructor y una base del metodo de movimiento.

---
+++ src/AgenteBase.java
@@ -0,0 +1,22 @@
+public class AgenteBase {
+    private int[] posicion;
+    private String estado;
+    public AgenteBase(){
+        this.posicion[0]=0;
+        this.posicion[1]=0;
+        this.estado="buscando";
+    }
+    public void movimiento(){
+        if (estado=="buscando"){
+            //if (casilla frente, atras, izq o der, == validas){
+                //mover
+            //}
+        }
+        else if (estado=="entregando"){
+            //if (casilla frente, atras, izq o der, == validas){
+                //mover
+            //}
+        }
+    }
+}
+
